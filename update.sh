033fa4ceec0518e3b4fad11674075327d4393bb4
Kim Sondrup <kim@sondrup.xyz>
2017-12-01 19:54:28 +0100

    Initial Magento2 Dockerfiles

--- update.sh
+++ update.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 set -e
 
-# https://secure.php.net/gpg-keys.php
-declare -A gpgKeys=(
-	# https://wiki.php.net/todo/php71
-	# davey & krakjoe
-	# https://secure.php.net/downloads.php#gpg-7.1
-	# https://secure.php.net/gpg-keys.php#gpg-7.1
-	[7.1]='A917B1ECDA84AEC2B568FED6F50ABC807BD5DCD0 528995BFEDFBA7191D46839EF9BA0ADA31CBD89E'
-
-	# https://wiki.php.net/todo/php70
-	# ab & tyrael
-	# https://secure.php.net/downloads.php#gpg-7.0
-	# https://secure.php.net/gpg-keys.php#gpg-7.0
-	[7.0]='1A4E8B7277C42E53DBA9C7B9BCAA30EA9C0D5763 6E4F6AB321FDC07F2C332E3AC2BF0BC433CFC8B3'
-)
-# see https://secure.php.net/downloads.php
-
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( "$@" )
@@ -92,23 +76,7 @@ for version in "${versions[@]}"; do
 	# format of "possibles" array entries is "VERSION URL.TAR.XZ URL.TAR.XZ.ASC SHA256 MD5" (each value shell quoted)
 	#   see the "apiJqExpr" values above for more details
 	eval "possi=( ${possibles[0]} )"
-	fullVersion="${possi[0]}"
 	url="${possi[1]}"
-	ascUrl="${possi[2]}"
-	sha256="${possi[3]}"
-	md5="${possi[4]}"
-
-	gpgKey="${gpgKeys[$rcVersion]}"
-	if [ -z "$gpgKey" ]; then
-		echo >&2 "ERROR: missing GPG key fingerprint for $version"
-		echo >&2 "  try looking on https://secure.php.net/downloads.php#gpg-$version"
-		exit 1
-	fi
-
-	# if we don't have a .asc URL, let's see if we can figure one out :)
-	if [ -z "$ascUrl" ] && wget -q --spider "$url.asc"; then
-		ascUrl="$url.asc"
-	fi
 
 	dockerfiles=()
 
@@ -118,7 +86,6 @@ for version in "${versions[@]}"; do
 			echo "Skipping Alpine build"
 			continue
 		fi
-		alpineVer="${suite#alpine}"
 
 		baseDockerfile=Dockerfile-debian.template
 		if [ "${suite#alpine}" != "$suite" ]; then
@@ -139,48 +106,11 @@ for version in "${versions[@]}"; do
 				' "$version/$suite/$variant/Dockerfile"
 			fi
 
-			cp \
-				docker-php-entrypoint \
-				docker-php-ext-* \
-				docker-php-source \
-				"$version/$suite/$variant/"
-
-			if [ "$alpineVer" = '3.4' ]; then
-				sed -ri 's!libressl!openssl!g' "$version/$suite/$variant/Dockerfile"
-			fi
-			if [ "$majorVersion" = '5' ] || [ "$majorVersion" = '7' -a "$minorVersion" -lt '2' ] || [ "$suite" = 'jessie' ]; then
-				# argon2 password hashing is only supported in 7.2+ and stretch+
-				sed -ri '/argon2/d' "$version/$suite/$variant/Dockerfile"
-				# Alpine 3.7+ _should_ include an "argon2-dev" package, but we should cross that bridge when we come to it
-			fi
-
-			# automatic `-slim` for stretch
-			# TODO always add slim once jessie is removed
 			sed -ri \
-				-e 's!%%DEBIAN_SUITE%%!'"${suite/stretch/stretch-slim}"'!' \
-				-e 's!%%ALPINE_VERSION%%!'"$alpineVer"'!' \
+				-e 's!%%PHP_SUITE%%!'"$version-$variant-$suite"'!' \
 				"$version/$suite/$variant/Dockerfile"
 			dockerfiles+=( "$version/$suite/$variant/Dockerfile" )
 		done
-	done
-
-	(
-		set -x
-		sed -ri \
-			-e 's!%%PHP_VERSION%%!'"$fullVersion"'!' \
-			-e 's!%%GPG_KEYS%%!'"$gpgKey"'!' \
-			-e 's!%%PHP_URL%%!'"$url"'!' \
-			-e 's!%%PHP_ASC_URL%%!'"$ascUrl"'!' \
-			-e 's!%%PHP_SHA256%%!'"$sha256"'!' \
-			-e 's!%%PHP_MD5%%!'"$md5"'!' \
-			"${dockerfiles[@]}"
-	)
-
-	# update entrypoint commands
-	for dockerfile in "${dockerfiles[@]}"; do
-		cmd="$(awk '$1 == "CMD" { $1 = ""; print }' "$dockerfile" | tail -1 | jq --raw-output '.[0]')"
-		entrypoint="$(dirname "$dockerfile")/docker-php-entrypoint"
-		sed -i 's! php ! '"$cmd"' !g' "$entrypoint"
 	done
 
 	newTravisEnv=
